5ea7dd8d5aac351323c612c94882fe7a9ba8460e
lparolari <luca.parolari23@gmail.com>
2021-08-12 10:34:06 +0200

    feat(res/pikle-viewer): add --keys-only option

--- resources/pickle_viewer/pickle_viewer.py
+++ resources/pickle_viewer/pickle_viewer.py
@@ -3,15 +3,21 @@ import pickle
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A viewer for .pickle files.")
+    parser.add_argument("--keys-only", action="store_true", required=False,
+                        default=False, help="Whether to visualize only keys")
     parser.add_argument("--file", dest="file", required=False,
                         default="1000092795_0.pickle", help="A .pickle file")
 
     args = parser.parse_args()
 
     file = args.file
+    keys_only = args.keys_only
 
     with open(file, "rb") as f:
         x = pickle.load(f)
 
     for k, v in x.items():
-        print(f"{k} = {v}")
+        if keys_only:
+            print(k)
+        else:
+            print(f"{k} = {v}")
